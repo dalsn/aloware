a1589c9770e9cf0ad2b3bd120a7710965dcf9226
Dalhatu Njidda <dalsdnjidda@gmail.com>
2022-07-25 08:28:02 +0100

    update comment test cases

--- tests/Feature/CommentTest.php
+++ tests/Feature/CommentTest.php
@@ -15,7 +15,7 @@ class CommentTest extends TestCase
     {
         $comment = factory(Comment::class, 5)->create();
 
-        $response = $this->json('GET', route('comment.index'));
+        $response = $this->json('GET', route('comments.index'));
 
         $response->assertStatus(200);
         $this->assertEquals(5, count($response->json()));
@@ -25,7 +25,7 @@ class CommentTest extends TestCase
     {
         $comment = factory(Comment::class)->create();
 
-        $response = $this->json('GET', route('comment.show', $comment->id));
+        $response = $this->json('GET', route('comments.show', $comment->id));
 
         $response->assertStatus(200);
         $this->assertEquals(1, count($response->json()));
@@ -37,7 +37,7 @@ class CommentTest extends TestCase
     {
         $comment = factory(Comment::class)->make();
 
-        $response = $this->json('POST', route('comment.create'), $comment->toArray());
+        $response = $this->json('POST', route('comments.store'), $comment->toArray());
         $response->assertStatus(200);
 
         $comments = Comment::all();
@@ -46,15 +46,17 @@ class CommentTest extends TestCase
 
     public function testCanRespondToAComment()
     {
+        $this->withoutExceptionHandling();
+        
         $parentComment = factory(Comment::class)->create();
         $comment = factory(Comment::class)->make([
             'parent_id' => $parentComment->id,
         ]);
 
-        $response = $this->json('POST', route('comment.create'), $comment->toArray());
+        $response = $this->json('POST', route('comments.store'), $comment->toArray());
 
         $response->assertStatus(200);
-        $this->assertCount(1, $parentComment->comments);
+        $this->assertCount(1, $parentComment->comments->toArray());
         $this->assertEquals($parentComment->comments()->first()->body, $comment->body);
     }
 }
